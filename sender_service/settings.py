e8f3c8360c1c5f8ebb7c75554a6124148151adea
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-12-31 20:46:57 +0530

    try

--- sender_service/settings.py
+++ sender_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5(tw1qt(*83i2ygr+i0&4rvx9j(ga@vf&916)3tiu(ac85^v-d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
