420f2d007c653bd2efa0716c679a9dba4c61daa6
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-01-01 14:16:31 +0530

    omkars url

--- sender/tasks.py
+++ sender/tasks.py
@@ -3,11 +3,12 @@ import time
 import random
 import threading
 
-RECEIVER_URL = "https://receiver-service-8nwe.onrender.com/api/receive/"
+RECEIVER_URL2 = "http://127.0.0.1:8000/api/receive/"
+RECEIVER_URL = "https://expo-t2be.onrender.com/"
 
 def send_random_requests():
     while True:
-        wait_time = random.randint(60, 120)  # 5–10 minutes
+        wait_time = random.randint(300, 400)  # 5–10 minutes
         print(f"⏳ Waiting {wait_time} seconds...")
 
         time.sleep(wait_time)
@@ -19,7 +20,8 @@ def send_random_requests():
         }
 
         try:
-            response = requests.post(RECEIVER_URL, json=payload, timeout=60)
+            response = requests.post(RECEIVER_URL, json=payload, timeout=2)
+            response = requests.post(RECEIVER_URL2, json=payload, timeout=2)
             print("✅ Sent:", response.status_code)
         except Exception as e:
             print("❌ Error sending request:", e)
