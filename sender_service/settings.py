a2f445dccfb92eb3610d32e8b2da92ef80b0202b
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-01-28 13:06:33 +0530

    add landing page and error templates; update settings for template directory

--- sender_service/settings.py
+++ sender_service/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5(tw1qt(*83i2ygr+i0&4rvx9j(ga@vf&916)3tiu(ac85^v-d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
